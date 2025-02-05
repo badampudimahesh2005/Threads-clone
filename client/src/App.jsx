dcb1f9cc026e7654e74119665c2265ca4b464dbd
Mahesh@Webdev <maheshbadampudi31@gmail.com>
2025-02-05 12:43:12 +0530

    Register Ui

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,17 +5,19 @@ import Header from './components/common/Header'
 import Home from './pages/protected/Home'
 import Search from './pages/protected/Search'
 import Error from './pages/Error'
+import Register from './pages/Register'
 const App = () => {
   return (
     <>
-    <BrowserRouter>
+    <Register />
+    {/* <BrowserRouter>
     <Header />
     <Routes>
     <Route  path="/" element={<Home />} />
     <Route path="/search" element={<Search />} />
     <Route path="*" element={<Error />} />
     </Routes>
-    </BrowserRouter>
+    </BrowserRouter> */}
     </>
   )
 }
---
+++ client/src/pages/Register.jsx
@@ -0,0 +1,54 @@
+import React from 'react'
+import { Button, Stack, TextField, Typography } from '@mui/material'
+const Register = () => {
+  return (
+    <>
+    <Stack 
+    width={"100%"}
+    height={"100vh"}
+    flexDirection={"row"}   
+    justifyContent={"center"}
+    alignItems={"center"}
+    sx={{
+        backgroundImage:"url('/register-bg.webp')",
+        backgroundRepeat:"no-repeat",
+        backgroundSize:"100% 600px",
+        
+    }}
+    >
+        <Stack 
+        flexDirection={"column"}
+        width={'40%'}
+        gap={2}
+        mt={20}
+        >
+            <Typography variant={"h4"}
+             fontSize={'1.5rem'}
+             fontWeight={'bold'}
+             alignSelf={'center'}
+            >Login with email</Typography>
+            <TextField variant='outlined' label='Enter your name' />
+            <TextField variant='outlined' label='Enter your email' type='email' />
+            <TextField variant='outlined' label='Enter your password' type='password' />
+            <Button size='large'
+            sx={{
+                width:'100%',
+                height:52,
+                bgcolor:'green',
+                color:'white',
+                fontSize:'1rem',
+                ":hover":{
+                    bgcolor:'blue',
+                    cursor:'pointer'
+                }
+            }}>Sign Up</Button>
+            <Typography variant='subtitle1' fontSize={'1.3rem'} alignSelf='center'>Already have an account? <span className='login-link' >Login</span></Typography>
+
+        </Stack>
+
+    </Stack>
+    </>
+  )
+}
+
+export default Register
